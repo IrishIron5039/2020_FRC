06a471b07991b7cda526d4d60fbd4e765a9be88a
IrishIron5039 <61030692+IrishIron5039@users.noreply.github.com>
2020-02-22 15:48:13 -0500

    Update Robot.cpp

--- 2020RobotCode - Pt 1/src/main/cpp/Robot.cpp
+++ 2020RobotCode - Pt 1/src/main/cpp/Robot.cpp
@@ -38,7 +38,7 @@ Shifting gears (Toggle): getRawButton(1)
 #include <frc/WPILib.h> //Including everything you need for the robot code to work
 
 class Robot : public frc::IterativeRobot {
-public:
+	public:
 	frc::ADXRS450_Gyro *gyro = new frc::ADXRS450_Gyro();
 	
 	WPI_TalonSRX *left = new WPI_TalonSRX(0); 
@@ -53,21 +53,21 @@ public:
 	WPI_TalonSRX *ballWrist = new WPI_TalonSRX(9); 
 	WPI_TalonSRX *controlPanelSpin = new WPI_TalonSRX(10); 
 
-	// Kiran
-	//frc::Encoder elevatorEncoder{0, 1}; // This is hopefully initializing a rotary quadrature encoder
-	//bool elevatorUp = false;
 
 	frc::DoubleSolenoid changeWheelSpeed {3, 5}; //Pneumatics, connected to spot 1 and 5 to the powerboard next to the battery
-	//frc::DoubleSolenoid endAffector {1, 6}; //Connected to spot 1 and 6
-	//frc::DoubleSolenoid hatch {2, 3}; // Connected to spot 2 and 3
+	//frc::DoubleSolenoid endAffector {2, 6}; //Connected to spot 1 and 6
+	//frc::DoubleSolenoid hatch {1, 7}; // Connected to spot 2 and 3
 
 	frc::DifferentialDrive *drive = new frc::DifferentialDrive(*left, *right); //Setting drive to 1 joystick, adds the left and right joystick
 
 	frc::Joystick *joy = new frc::Joystick(0);
 	frc::Joystick *gamePad = new frc::Joystick(1);
 
+	frc::DigitalInput topOfElevator = frc::DigitalInput(0);
+	frc::DigitalInput bottomOfElevator = frc::DigitalInput(1);
 
-	float angleElevator = 0; //Placeholder value
+
+	float angleElevator = 0; //Placeholder value 
 	bool wheelSpeedFast = true;
 	int lowestElevatorPosition = elevator->GetSensorCollection().GetPulseWidthPosition() + 500;
 	int highestElevatorPosition = lowestElevatorPosition + 4096*2.5 - 1000;
@@ -79,12 +79,12 @@ public:
 		camera.SetResolution(320,240);
 		camera2.SetResolution(320,240);
 
-		left->SetInverted(true);
-		right->SetInverted(true);
-		leftFollower->SetInverted(true);
-		rightFollower->SetInverted(true);
-		leftFollower2->SetInverted(true);
-		rightFollower2->SetInverted(true);
+		left->SetInverted(false);
+		right->SetInverted(false);
+		leftFollower->SetInverted(false);
+		rightFollower->SetInverted(false);
+		leftFollower2->SetInverted(false);
+		rightFollower2->SetInverted(false);
 		
 		
 
@@ -93,14 +93,14 @@ public:
 		leftFollower2->Follow(*left);
 		rightFollower2->Follow(*right);
 		
-		//elevator2->Follow(*elevator);
+		elevator2->Follow(*elevator);
 
 		ballWrist->ConfigSelectedFeedbackSensor(FeedbackDevice::Analog, 0, 0);
 		ballWrist->ConfigSetParameter(ParamEnum::eFeedbackNotContinuous, 1, 0x00,0x00, 0x00);
 		ballWrist->SetSensorPhase(true);
 
 		ballWrist->Config_kF(0, 0.0, 0);
-		ballWrist->Config_kP(0, 1, 0);
+		ballWrist->Config_kP(0, 1, 0);-
 		ballWrist->Config_kI(0, 0.0, 0);
 		ballWrist->Config_kD(0, 0.0, 0);
 
@@ -111,80 +111,63 @@ public:
 		//hatch.Set(frc::DoubleSolenoid::Value::kForward);
 		//endAffector.Set(frc::DoubleSolenoid::Value::kForward);
 
-		// Kiran
-		//elevatorEncoder.SetDistancePerPulse(1./256.);
-		//elevatorEncoder.Reset();
-
 	}
 
-	void AutonomousInit() {
+	void AutonomousInit () {
 		gyro->Reset();
 	}
 
-
-
-void AutonomousPeriodic() {
-	//drive->ArcadeDrive(joy->GetY(), -joy->GetX(), true);
-	//elevator->Set(1);
-	/*if (gamePad->GetRawButton(1)) {
-		angleElevator = 1000;//Placeholder
-		elevator->Config_kP(0, 1, 0);
+	void AutonomousPeriodic() {
+		
+		drive->ArcadeDrive(joy->GetY(), -joy->GetX(), true);
+		
 	}
-	if (gamePad->GetRawButton(2)) {
-		angleElevator = 0;//Placeholder
-		elevator->Config_kP(0, 1, 0);
-	}
-	elevator->Set(ControlMode::Position, angleElevator);*/
-}
+
 	void TeleopInit() {
 		gyro->Reset();
 	}
 
 	void TeleopPeriodic() {
 		drive->ArcadeDrive(joy->GetY(), -joy->GetX(), true);
-		if(gamePad->GetRawButton(5)){
-			if(!(elevator->GetSensorCollection().GetPulseWidthPosition() < lowestElevatorPosition)){
-				elevator->Set (-0.25);
-			}
-			else elevator->Set (0);
+		if(gamePad->GetRawButton(5)){//Down
+			//if(!(elevator->GetSensorCollection().GetPulseWidthPosition() < lowestElevatorPosition)){
+				if(bottomOfElevator.Get()){
+					elevator->Set (-0.25);
+				}
+				else elevator->Set (0);
+			//}
+			//else elevator->Set (0);
 		}
-		else if(gamePad->GetRawButton(6)){
-			if(!(elevator->GetSensorCollection().GetPulseWidthPosition() > highestElevatorPosition)){
-				elevator->Set (0.25);
-			}
-			else elevator->Set (0);
+		else if(gamePad->GetRawButton(6)){ //Up
+			//if(!(elevator->GetSensorCollection().GetPulseWidthPosition() > highestElevatorPosition)){
+				if(topOfElevator.Get()){
+					elevator->Set (0.25);
+				}
+				else elevator->Set (0);
+			//}
+			//else elevator->Set (0);
 		}
-		//elevator->Set (0.5 * (gamePad->GetRawButton(5)-gamePad->GetRawButton(6)));
+		else elevator->Set (0);
 		//std::cout << "GetPulseWidthPosition: " << elevator->GetSensorCollection().GetPulseWidthPosition() << "\n";
-		std::cout << "Left: " << left->GetSensorCollection().GetPulseWidthPosition() << "\n";
-		std::cout << "Right: " << right->GetSensorCollection().GetPulseWidthPosition() << "\n";
+		//std::cout << "Left: " << left->GetSensorCollection().GetPulseWidthPosition() << "\n";
+		//std::cout << "Right: " << right->GetSensorCollection().GetPulseWidthPosition() << "\n";
 		
 		if (joy->GetRawButton(1)) changeWheelSpeed.Set(frc::DoubleSolenoid::Value::kReverse);
 		
 		else changeWheelSpeed.Set(frc::DoubleSolenoid::Value::kForward);
 		
-		/*if(gamePad->GetRawButton(1)){
-			elevatorUp = !elevatorUp;
-		}
-
-		// Kiran
-		if(elevatorUp && elevatorEncoder.GetDistance < 2.75){
-			elevator->Set(1);
-		}
-		else if(!elevatorUp && elevatorEncoder.GetDistance > 0.25){
-			elevator->Set(-1);
-		}
-		*/
 	}
 
-	void TestPeriodic() {}
+	void TestPeriodic() {
+		drive->ArcadeDrive(joy->GetY(), -joy->GetX(), true);
+	}
 
-private:
-	frc::LiveWindow& m_lw = *frc::LiveWindow::GetInstance();
-	frc::SendableChooser<std::string> m_chooser;
-	const std::string kAutoNameDefault = "Default";
-	const std::string kAutoNameCustom = "My Auto";
-	std::string m_autoSelected;
+	private:
+		frc::LiveWindow& m_lw = *frc::LiveWindow::GetInstance();
+		frc::SendableChooser<std::string> m_chooser;
+		const std::string kAutoNameDefault = "Default";
+		const std::string kAutoNameCustom = "My Auto";
+		std::string m_autoSelected;
 };
 
 #ifndef RUNNING_FRC_TESTS
